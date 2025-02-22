4bc09d2f3e3930bdae4db3ca6cd58a2949200d00
Adolfo Moreno <atgmo12@gmail.com>
2025-02-22 20:36:25 +0000

    fix preload issue

--- preload.js
+++ preload.js
@@ -1,8 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electronAPI', {
+contextBridge.exposeInMainWorld('electron', {
   selectFolder: () => ipcRenderer.invoke('select-folder'),
   selectSQLiteFile: () => ipcRenderer.invoke('select-sqlite'),
   selectKoboPath: () => ipcRenderer.invoke('select-kobo'),
-  openHelp: () => ipcRenderer.invoke('help-button')
+  openHelp: () => ipcRenderer.invoke('help-button'),
 });
--- renderer.js
+++ renderer.js
@@ -1,36 +1,36 @@
 document.getElementById('select-folder').addEventListener('click', async () => {
-    const data = await window.electron.selectFolder();
-  
-    if (data.length > 0) {
-      localStorage.setItem('annotations', JSON.stringify(data));
-      window.location.href = 'ui/results_screen/results.html'; // Navigate to results page
-    } else {
-      alert('No annotations found or action was cancelled.');
-    }
-  });
+  const data = await window.electron.selectFolder();
 
-  document.getElementById('select-sqlite').addEventListener('click', async () => {  
-    const data = await window.electron.selectSQLiteFile();
+  if (data.length > 0) {
+    localStorage.setItem('annotations', JSON.stringify(data));
+    window.location.href = 'ui/results_screen/results.html'; // Navigate to results page
+  } else {
+    alert('No annotations found or action was cancelled.');
+  }
+});
 
-    if (data.length > 0) {
-      localStorage.setItem('sql_annotations', JSON.stringify(data));
-      window.location.href = 'ui/results_screen/results.html'; // Navigate to results page
-    } else {
-      alert('No annotations found or action was cancelled.');
-    }
-  });
-  
-  document.getElementById('select-kobo').addEventListener('click', async () => {
-    const data = await window.electron.selectKoboPath();
-  
-    if (data.length > 0) {
-      localStorage.setItem('sql_annotations', JSON.stringify(data));
-      window.location.href = 'ui/results_screen/results.html'; // Navigate to results page
-    } else {
-      alert('No annotations found or action was cancelled.');
-    }
-  });
-  
-  document.getElementById('help-button').addEventListener('click', async () => {  
-    alert('Not yet implemented');
-  });
+document.getElementById('select-sqlite').addEventListener('click', async () => {
+  const data = await window.electron.selectSQLiteFile();
+
+  if (data.length > 0) {
+    localStorage.setItem('sql_annotations', JSON.stringify(data));
+    window.location.href = 'ui/results_screen/results.html'; // Navigate to results page
+  } else {
+    alert('No annotations found or action was cancelled.');
+  }
+});
+
+document.getElementById('select-kobo').addEventListener('click', async () => {
+  const data = await window.electron.selectKoboPath();
+
+  if (data.length > 0) {
+    localStorage.setItem('sql_annotations', JSON.stringify(data));
+    window.location.href = 'ui/results_screen/results.html'; // Navigate to results page
+  } else {
+    alert('No annotations found or action was cancelled.');
+  }
+});
+
+document.getElementById('help-button').addEventListener('click', async () => {
+  alert('Not yet implemented');
+});
